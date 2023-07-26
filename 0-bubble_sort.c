22b2e0b0b43c3b7162cdf5acb355a2be152bb562
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-07-26 01:44:31 +0000

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,25 +6,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, idx;
-    int tmp;
+	size_t i, idx;
+	int tmp;
 
-    if (array == NULL || size < 2)
-        return;
-    
-    i = 0;
-    while (i < size - 1)
-    {
-        for (idx = 0; idx < size - i - 1; idx++)
-        {
-            if (array[idx] > array[idx + 1])
-            {
-                tmp = array[idx];
-                array[idx] = array[idx + 1];
-                array[idx + 1] = tmp;
-                print_array(array, size);
-            }
-        }
-        i++;
-    }
+	if (array == NULL || size < 2)
+		return;
+
+	i = 0;
+	while (i < size - 1)
+	{
+		for (idx = 0; idx < size - i - 1; idx++)
+		{
+			if (array[idx] > array[idx + 1])
+			{
+				tmp = array[idx];
+				array[idx] = array[idx + 1];
+				array[idx + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
 }
